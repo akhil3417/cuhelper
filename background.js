dda148ea16ac3c540593d0b94f2e690e660c65c7
akhil3417 <akhilpratapsingh3417@gmail.com>
2023-07-08 17:35:02 +0530

    listen ,inject search buttons when the URL matches

---
+++ background.js
@@ -0,0 +1,15 @@
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.tabs.executeScript(tab.id, {
+    code: `
+      // Bookmarklet code goes here
+    `,
+  });
+});
+// Background listener to inject search buttons when the URL matches the pattern
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status === "complete" && tab.url) {
+    if (tab.url.includes("http:culms.cuidol.in/mod/quiz/")) {
+      chrome.tabs.executeScript(tabId, { file: "mcqsearchui.js" });
+    }
+  }
+});
