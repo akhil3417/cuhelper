c03fdd19c5fd2afbf59f23927c28f9cc99c5b62b
akhil3417 <akhilpratapsingh3417@gmail.com>
2023-07-08 17:31:32 +0530

    add click event listener for singelsearchui button

--- popup.js
+++ popup.js
@@ -3,3 +3,9 @@ document
   .addEventListener("click", function () {
     chrome.tabs.executeScript({ file: "searchallques.js" });
   });
+
+document
+  .getElementById("searchsingleQuestionsButton")
+  .addEventListener("click", function () {
+    chrome.tabs.executeScript({ file: "mcqsearchui.js" });
+  });
